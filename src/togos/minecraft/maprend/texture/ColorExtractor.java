0f5cd36171cc8d7b33415c982ce3fa99dea753e8
Klaus-Peter Hammerschmidt <klahammer@web.de>
2013-04-06 16:52:13 +0200

    Move ColorExtractor

--- src/togos/minecraft/maprend/texture/ColorExtractor.java
+++ src/togos/minecraft/maprend/texture/ColorExtractor.java
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU General Public License along with this program; 
  * if not, see <http://www.gnu.org/licenses/>.
  */
-package de.klahammer.minecraft.maprend.colorextract;
+package togos.minecraft.maprend.texture;
 
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
